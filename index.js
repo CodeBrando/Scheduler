197c869edf9624ee34420bdaa910c2c44d5651a5
Martín Braga <martin.braga252@gmail.com>
2022-09-12 23:13:42 -0300

    Primera interacción con el html

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ let opcion2
 let opcion3 = 1
 let horasUsadas = 0
 let horasLibres = 0
+let porcentaje
 let tiempoLibre = 0
+let tareas = []
+const selectTag = document.getElementById('listaDeTareas')
 
 class Tarea{
     constructor(nombre, descripcion, recordatorio, hora, duracion){
@@ -29,6 +32,11 @@ function duracionDeTarea(tarea){
     return tarea.duracion;
 }
 
+function calcularTiempoLibre(horasUsadas){
+    horasLibres = 24 - horasUsadas
+    tiempoLibre = (horasUsadas*100)/24
+    return tiempoLibre
+}
 
 
 // function compare(a, b) {
@@ -39,139 +47,159 @@ function duracionDeTarea(tarea){
 //       return 1;
 //     }
 //     return 0;
-//   }
+// }
+
+let tareasOrdenadas = tareas.sort((obj1, obj2)=>obj1.hora - obj2.hora) 
+// No estoy pudiendo ordenar la lista por hora, no entiendo por qué no puedo hacer que la menor hora aparezca antes en la lista
+
+
+function agregarTareas(tarea){
+    tareas.push(tarea)
+    return tareas;
+}
+
+
 
 const pasearAlPerro = new Tarea("Pasear al perro", "Pasear al perro por el barrio durante aproximadamente media hora.", "Llevar bolsas de nylon.")
 const estudiar = new Tarea("Estudiar", "Ponerse al día con las clases sin ver hasta el momento; practicar al menos una vez lo ya visto.", "Chequear qur no haya desafíos por entregar.")
 const ocio = new Tarea("Tiempo libre", "Tiempo de descanso  hasta la próxima tarea", "Ponerse al día con dailies en varios juegos.")
 let nuevaTarea = new Tarea()
 
-
-let tareas = []
-
-
-alert("Bienvenido a ScheTool!")
-alert("Aquí podrás crear horarios que se adopten a tu día a día.")
+tareas.push(pasearAlPerro, estudiar, ocio)
 
 
 
-while(run === true){
-    let tarea = parseInt(prompt("Ingrese la tarea que desea añadir: 1- Pasear al perro, 2- Estudiar, 3- Tiempo libre, 4- Crear una nueva tarea."))
+
+
+// alert("Bienvenido a ScheTool!")
+// alert("Aquí podrás crear horarios que se adopten a tu día a día.")
+
+
+tareas.forEach(tarea=>{
+    const option = document.createElement('option')
+    option.innerText = `${tarea.nombre}`
+    selectTag.append(option)
+})
+
+
+// while(run === true){
+//     let tarea = parseInt(prompt("Ingrese la tarea que desea añadir: 1- Pasear al perro, 2- Estudiar, 3- Tiempo libre, 4- Crear una nueva tarea."))
     
     
 
-    if (tarea === 1){
-        horaDeTarea(pasearAlPerro)
-        if(pasearAlPerro.hora > 23.59){
-            do{
-                alert("La hora no puede superar las 23.59 hs")
-                horaDeTarea(pasearAlPerro)
-                }
-                while(pasearAlPerro.hora > 23.59)
-                continue
+//     if (tarea === 1){
+//         horaDeTarea(pasearAlPerro)
+//         if(pasearAlPerro.hora > 23.59){
+//             do{
+//                 alert("La hora no puede superar las 23.59 hs")
+//                 horaDeTarea(pasearAlPerro)
+//                 }
+//                 while(pasearAlPerro.hora > 23.59)
+//                 continue
 
-        }
-        duracionDeTarea(pasearAlPerro)
-        if(pasearAlPerro.duracion > 23.59){
-            do{
-                alert("La duración no puede superar las 23.59 hs")
-                duracionDeTarea(pasearAlPerro)
-                }
-                while(pasearAlPerro.duracion > 23.59)
-                continue
-        }
-        horasUsadas = horasUsadas += pasearAlPerro.duracion
-        tareas += JSON.stringify(pasearAlPerro)
+//         }
+//         duracionDeTarea(pasearAlPerro)
+//         if(pasearAlPerro.duracion > 23.59){
+//             do{
+//                 alert("La duración no puede superar las 23.59 hs")
+//                 duracionDeTarea(pasearAlPerro)
+//                 }
+//                 while(pasearAlPerro.duracion > 23.59)
+//                 continue
+//         }
+//         horasUsadas = horasUsadas += pasearAlPerro.duracion
+//         agregarTareas(pasearAlPerro);
+        
         
 
-    } else if (tarea === 2){
-        horaDeTarea(estudiar)
-        if(estudiar.hora > 23.59){
-            do{
-                alert("La hora no puede superar las 23.59 hs")
-                horaDeTarea(estudiar)
-                }
-                while(estudiar.hora > 23.59)
-                continue
+//     } else if (tarea === 2){
+//         horaDeTarea(estudiar)
+//         if(estudiar.hora > 23.59){
+//             do{
+//                 alert("La hora no puede superar las 23.59 hs")
+//                 horaDeTarea(estudiar)
+//                 }
+//                 while(estudiar.hora > 23.59)
+//                 continue
 
-        }
-        duracionDeTarea(estudiar)
-        if(estudiar.duracion > 23.59){
-            do{
-            alert("La duración no puede superar las 23.59 hs")
-            duracionDeTarea(estudiar)
-            }
-            while(estudiar.duracion > 23.59)
-            continue
-        }
-        horasUsadas = horasUsadas += estudiar.duracion
-        tareas += JSON.stringify(estudiar)
+//         }
+//         duracionDeTarea(estudiar)
+//         if(estudiar.duracion > 23.59){
+//             do{
+//             alert("La duración no puede superar las 23.59 hs")
+//             duracionDeTarea(estudiar)
+//             }
+//             while(estudiar.duracion > 23.59)
+//             continue
+//         }
+//         horasUsadas = horasUsadas += estudiar.duracion
+//         agregarTareas(estudiar);
 
-    } else if (tarea === 3){
-        horaDeTarea(ocio)
-        if(ocio.hora > 23.59){
-            do{
-                alert("La hora no puede superar las 23.59 hs")
-                horaDeTarea(ocio)
-                }
-                while(ocio.hora > 23.59)
-                continue
+//     } else if (tarea === 3){
+//         horaDeTarea(ocio)
+//         if(ocio.hora > 23.59){
+//             do{
+//                 alert("La hora no puede superar las 23.59 hs")
+//                 horaDeTarea(ocio)
+//                 }
+//                 while(ocio.hora > 23.59)
+//                 continue
 
-        }
-        duracionDeTarea(ocio)
-        if(ocio.duracion > 23.59){
-            do{
-                alert("La duración no puede superar las 23.59 hs")
-                duracionDeTarea(ocio)
-                }
-                while(ocio.duracion > 23.59)
-                continue
-        }
-        horasUsadas = horasUsadas += ocio.duracion
-        tareas += JSON.stringify(ocio)
+//         }
+//         duracionDeTarea(ocio)
+//         if(ocio.duracion > 23.59){
+//             do{
+//                 alert("La duración no puede superar las 23.59 hs")
+//                 duracionDeTarea(ocio)
+//                 }
+//                 while(ocio.duracion > 23.59)
+//                 continue
+//         }
+//         horasUsadas = horasUsadas += ocio.duracion
+//         agregarTareas(ocio);
 
-    } else if (tarea === 4){
-        let nuevaTarea = new Tarea(prompt("Introduce el nombre de la tarea: "), prompt("Describe la tarea: "), prompt("Añade un recordatorio si fuese necesario: "))  
-        horaDeTarea(nuevaTarea)
-        if(nuevaTarea.hora > 23.59){
-            do{
-                alert("La hora no puede superar las 23.59 hs")
-                horaDeTarea(nuevaTarea)
-                }
-                while(nuevaTarea.hora > 23.59)
-                continue
+//     } else if (tarea === 4){
+//         let nuevaTarea = new Tarea(prompt("Introduce el nombre de la tarea: "), prompt("Describe la tarea: "), prompt("Añade un recordatorio si fuese necesario: "))  
+//         horaDeTarea(nuevaTarea)
+//         if(nuevaTarea.hora > 23.59){
+//             do{
+//                 alert("La hora no puede superar las 23.59 hs")
+//                 horaDeTarea(nuevaTarea)
+//                 }
+//                 while(nuevaTarea.hora > 23.59)
+//                 continue
 
-        }
-        duracionDeTarea(nuevaTarea)
-        if(nuevaTarea.duracion > 23.59){
-            do{
-                alert("La duración no puede superar las 23.59 hs")
-                duracionDeTarea(nuevaTarea)
-                }
-                while(nuevaTarea.duracion > 23.59)
-                continue
-        }
-        horasUsadas = horasUsadas += nuevaTarea.duracion
-        tareas += JSON.stringify(nuevaTarea)
+//         }
+//         duracionDeTarea(nuevaTarea)
+//         if(nuevaTarea.duracion > 23.59){
+//             do{
+//                 alert("La duración no puede superar las 23.59 hs")
+//                 duracionDeTarea(nuevaTarea)
+//                 }
+//                 while(nuevaTarea.duracion > 23.59)
+//                 continue
+//         }
+//         horasUsadas = horasUsadas += nuevaTarea.duracion
+//         agregarTareas(nuevaTarea);
 
         
-    } else {
-        tarea = parseInt(prompt("Ingrese la tarea que desea añadir: 1- Pasear al perro, 2- Estudiar, 3- Tiempo libre, 4- Crear una nueva tarea."))
-        continue
-    }
+//     } else {
+//         tarea = parseInt(prompt("Ingrese la tarea que desea añadir: 1- Pasear al perro, 2- Estudiar, 3- Tiempo libre, 4- Crear una nueva tarea."))
+//         continue
+//     }
 
-    opcion = parseInt(prompt("Desea ver sus tareas? 1- Si, 2- No"))
-    if(opcion === 1){
-        alert(tareas)
-    } 
+//     opcion = parseInt(prompt("Desea ver sus tareas? 1- Si, 2- No"))
+//     if(opcion === 1){
+//         alert(JSON.stringify(tareasOrdenadas))
+//     } 
 
 
     
 
-    opcion2 = parseInt(prompt("Desea seguir añadiendo tareas? 1- Si, 2- No"))
-    if (opcion2 === 2){
-        run = false
-    }
+//     opcion2 = parseInt(prompt("Desea seguir añadiendo tareas? 1- Si, 2- No"))
+//     if (opcion2 === 2){
+//         run = false
+//     }
 
     // if (horasUsadas > 23.59){
     //     alert("Las horas usadas no pueden superar las 23.59")
@@ -181,10 +209,10 @@ while(run === true){
 
     // Me gustaría poder limitar las horas del día, pero no sabría cómo hacer para que el usuario decida qué tarea eliminar cuando se pasa de horas
     
-}
+// }
 
-alert("Su cantidad de horas usadas es de: " + " " + horasUsadas)
-
+// alert("Su cantidad de horas usadas es de: " + " " + horasUsadas)
+// alert("Tiene un %" + " " + calcularTiempoLibre(horasUsadas) + " " + "de tiempo libre en el día.")
 
 
 
